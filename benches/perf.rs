cdfaa7436c014e05b39b0df06956ec99a89fbfc3
jdidion <github@didion.net>
2025-01-29 12:09:51 -0800

    fix panic during normal sentinel drop; add benches

---
+++ benches/perf.rs
@@ -0,0 +1,31 @@
+use beekeeper::bee::stock::EchoWorker;
+use beekeeper::hive::{outcome_channel, Builder};
+use divan::{bench, black_box_drop, AllocProfiler, Bencher};
+use itertools::iproduct;
+
+#[global_allocator]
+static ALLOC: AllocProfiler = AllocProfiler::system();
+
+const THREADS: &[usize] = &[1, 4, 8, 16];
+const TASKS: &[usize] = &[1, 100, 10_000, 1_000_000];
+
+#[bench(args = iproduct!(THREADS, TASKS))]
+fn bench_apply_short_task(bencher: Bencher, (num_threads, num_tasks): (&usize, &usize)) {
+    let hive = Builder::new()
+        .num_threads(*num_threads)
+        .build_with_default::<EchoWorker<usize>>();
+    bencher.bench_local(|| {
+        let (tx, rx) = outcome_channel();
+        for i in 0..*num_tasks {
+            hive.apply_send(i, tx.clone());
+        }
+        hive.join();
+        rx.into_iter()
+            .take(*num_tasks)
+            .for_each(|outcome| black_box_drop(outcome));
+    })
+}
+
+fn main() {
+    divan::main();
+}
